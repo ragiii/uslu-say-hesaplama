acc8c0673c843c3e4e19f5838db7eabea82fc9e8
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-07 01:09:25 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+  
+     int x, n;
+
+     Scanner input =new Scanner(System.in);
+     System.out.print("Üssü alınacak sayıyı giriniz :");
+     x = inp.nextInt();
+
+     System.out.print("Üs sayıyı giriniz :");
+     n = inp.nextInt();
+
+     int total = 1  // Sonucun sıfır çıkmaması için toplam değer 1 alındı.
+
+     for(int i=1; i<=n; i++){
+            total = total * x; 
+        }
+        System.out.print("Result : " + total);
+
+      }
+     
+}
+      
+
